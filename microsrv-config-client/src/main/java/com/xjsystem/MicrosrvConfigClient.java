f3e3947f4ca39e01a555d96ff88e507719aba69e
xjxb21 <xiaojixiang99@163.com>
2017-03-22 02:04:58 +0800

    config pom

---
+++ microsrv-config-client/src/main/java/com/xjsystem/MicrosrvConfigClient.java
@@ -0,0 +1,40 @@
+package com.xjsystem;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class MicrosrvConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvConfigClient.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "spring cloud config client";
+    }
+
+    //测试Profile的获取值
+    @Value("${profile}")
+    private String profile;
+
+    @RequestMapping("/getProfile")
+    public String getProfile() {
+        return this.profile;
+    }
+
+    //测试Profile的获取值
+    @Value("${profile2}")
+    private String profile2;
+
+    @RequestMapping("/getProfile2")
+    public String getProfile2() {
+        return this.profile2;
+    }
+}
