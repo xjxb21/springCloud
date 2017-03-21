3f7be7be5155470f8f9bc7fe4658e63eb2795b50
xjxb21 <xiaojixiang99@163.com>
2017-03-22 02:03:19 +0800

    config pom

--- microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
+++ microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
@@ -2,12 +2,32 @@ package com.xjsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * web 访问格式
+ /{application}/{profile}[/{label}]
+ /{application}-{profile}.yml
+ /{label}/{application}-{profile}.yml
+ /{application}-{profile}.properties
+ /{label}/{application}-{profile}.properties
+ */
 
 @SpringBootApplication
+@RestController
+@EnableConfigServer
 public class MicrosrvConfigServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrosrvConfigServer.class, args);
 	}
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
 }
