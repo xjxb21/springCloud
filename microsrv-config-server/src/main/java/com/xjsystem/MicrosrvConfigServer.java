92b6fa6c00ccfd6fe4ddab2af3defacc06bf0e01
xjxb21 <xiaojixiang99@163.com>
2017-03-21 19:38:23 +0800

    config pom

--- microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
+++ microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
@@ -2,11 +2,9 @@ package com.xjsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableEurekaServer
 public class MicrosrvConfigServer {
 
 	public static void main(String[] args) {
