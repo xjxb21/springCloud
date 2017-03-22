918730d3a9a22c681eb12fbf541076f6fc9cab0c
xjxb21 <xiaojixiang99@163.com>
2017-03-22 17:56:19 +0800

    git 配置动态刷新

--- microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
+++ microsrv-config-server/src/main/java/com/xjsystem/MicrosrvConfigServer.java
@@ -2,6 +2,7 @@ package com.xjsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableConfigServer
+@EnableDiscoveryClient
 public class MicrosrvConfigServer {
 
 	public static void main(String[] args) {
